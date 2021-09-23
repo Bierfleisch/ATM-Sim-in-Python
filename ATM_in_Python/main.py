3914025cbe26f812f08e98f7869233208473ad57
Bierfleisch <91275803+Bierfleisch@users.noreply.github.com>
2021-09-23 17:39:52 +0200

    Add files via upload

---
+++ ATM_in_Python/main.py
@@ -0,0 +1,41 @@
+import json
+from os.path import exists
+from shutil import copyfile
+
+
+def create_account(filename):
+    source = 'template.json'
+    destination = f'Accounts/{filename}.json'
+    copyfile(source, destination)
+
+
+def load_account(account_name):
+    path = f'Accounts/{account_name}.json'
+
+    path_exits = exists(path)
+
+    if not path_exits:
+        create_account(account_name)
+
+    with open(path, 'r') as file:
+        # load the json file and convert to python dictionary
+        account = json.load(file)
+
+        if not path_exits:
+            # if the dictionary was just created set the filename inside the file
+            account['accountName'] = account_name
+
+        return account
+
+
+def save_account(account):
+    with open(f'Accounts/{account["accountName"]}.json', 'w') as file:
+        # json.dump == convert python dictionary to file
+        json.dump(account, file)
+
+
+if __name__ == '__main__':
+    nemanja_account = load_account('Nemanja')
+    nemanja_account['balance'] = 1000000000
+    nemanja_account['balance'] -= 100
+    save_account(nemanja_account)
